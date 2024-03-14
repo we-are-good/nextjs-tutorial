9733d2b07ac1c5e8a6d6603c5bcdbdb4fdb28df7
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-14 11:11:37 +0900

    tutorial 6

---
+++ app/ui/font.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ // inter은 전체 글꼴
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
